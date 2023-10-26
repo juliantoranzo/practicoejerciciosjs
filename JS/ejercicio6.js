6574ac470ea3f0d317fe9783489d04ddb1e2f1a9
Julián Toranzo Navarro <julianalmacenamiento20@gmail.com>
2023-10-26 15:49:10 -0300

    i did exercise 6 but it is not complete, on branch ejercicio6

---
+++ JS/ejercicio6.js
@@ -0,0 +1,8 @@
+let numeroElegido1 = parseInt(prompt("Ingrese un núemero"));
+let numeroElegido2 = parseInt(prompt("Ingrese un núemero diferente al anterior"));
+if(numeroElegido1>numeroElegido2){
+    document.write("La variable numeroElegido1 es mayor a numeroElegido2")
+}
+if(numeroElegido1<numeroElegido2){
+    document.write("La variable numeroElegido2 es mayor a numeroElegido1")
+}
